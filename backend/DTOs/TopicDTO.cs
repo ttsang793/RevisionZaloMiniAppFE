6d00161564acfaafb8e53ec0104f0a482b7e8658
Tran Tuan Sang <ttsang793@gmail.com>
2025-09-30 10:49:20 +0700

    Split BE

--- backend/DTOs/TopicDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.DTOs;
-
-public class TopicDTO
-{
-    public string? Id { get; set; }
-    public string Name { get; set; } = null!;
-    public List<int> Classes { get; set; } = [];
-    public string SubjectId { get; set; } = null!;
-}
